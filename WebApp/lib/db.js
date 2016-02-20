2b7f25dc79c8331dd9c6aa9ea68c1eb474aa0e5a
uahengojr <uahengo@me.com>
2016-02-20 13:00:15 -0800

    Add db.js config code

---
+++ WebApp/lib/db.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var mongoose = require('mongoose');
+
+var db = fucntion(){
+	return {
+		config: function(conf){
+			mongoose.connect('mongodb://' + conf.host + '/' + conf.database);
+			var db = mongoose.connection;
+			db.on('error', console.error.bind(console, 'There was a connection error: Please try agian...'));
+			db.once('open', function callback(){
+				console.log('db connections is open');
+			});
+		}
+	};
+};
+
+module.exports = db();
