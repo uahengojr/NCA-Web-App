8a2eeb5de69d96abd1c796406f01bb493893e926
uahengojr <uahengo@me.com>
2016-03-05 21:16:10 -0800

    Add custom sign-in route handler.

--- WebApp/controllers/home.js
+++ WebApp/controllers/home.js
@@ -8,8 +8,10 @@ module.exports = function (router) {
     var model = new HomeModel();
 
     router.get('/', function (req, res) {
-        
-        
+        //console.log(req.session);
+        //console.log();
+		//console.log(req.user);
+		//res.locals.user = req.user;
         res.render('home', model);
         
         
--- WebApp/controllers/index.js
+++ WebApp/controllers/index.js
@@ -15,23 +15,43 @@ module.exports = function (router) {
         failureRedirect : '/', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
-	
+/*	
 	router.post('/signin', passport.authenticate('local-sign-in', {
         successRedirect : '/home', // redirect to the secure home section
         failureRedirect : '/', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
+*/	
+	// - Sign-in route handler for sign - //
+	router.post('/signin', 
+	passport.authenticate('local-sign-in'),
+	function(req, res) {
+		//If this function gets called, this means authentication was successful. 
+		//The 'req.user' contains the autheicated user.
+		
+		console.log(req.session);
+		console.log();
+		console.log("This is the user below:");
+		console.log(req.user);
+		console.log();
+		
+		// - The session object contains the user db id - //
+		req.session.passport.user;
+		req.user.passport.destroy();
+//		res.locals.user = req.user;
 
-
-	// - Logout route from application. - //
+		res.redirect('/home'); //req.user.username;
 	
-	router.get('/logout', function(req, res){
+	});
+	
+	// - Logout route from application. - //
+/*	router.get('/logout', function(req, res){
 	  //logic to clear session cookies and all info before exit.
-	  req.session.reset();
+	  req.session.destroy();
 	  req.logout();
 	  res.redirect('/');
 	});
-
+*/
 	// - This is a catch-all for requested pages that don't exist. - //
 	router.use(function (req,res) {
     	res.render('errors/404', {url:req.url});
