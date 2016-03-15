602df4ba243759bf3ff744a6a145e211d9f4007f
uahengojr <uahengo@me.com>
2016-03-15 11:30:57 -0700

    Add session store configuration file

---
+++ WebApp/lib/session.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var session = require('express-session'),
+	mongoStore = require('connect-mongodb-session');
+/*	
+module.exports = function(sessionConfig, mongoConfig) {
+		
+};
+
+*/
