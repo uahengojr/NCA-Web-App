08f784422af131463dca89b039b4838d4eb7cb4f
uahengojr <uahengo@me.com>
2016-03-16 00:02:40 -0700

    Add session logic

--- WebApp/lib/session.js
+++ WebApp/lib/session.js
@@ -2,9 +2,22 @@
 
 var session = require('express-session'),
 	mongoStore = require('connect-mongodb-session');
-/*	
-module.exports = function(sessionConfig, mongoConfig) {
-		
-};
 
-*/
+module.exports = function(sessionConfig, dbConfig){
+
+		var store = new mongoStore({
+        	uri: 'mongodb://' + dbConfig.host + '/' + dbConfig.db,
+        	collection: 'mySessions'
+		});
+    	
+		// Catch errors
+    	store.on('error', function(error) {
+			//Perhaps look into placing an error log...
+      	  	assert.ifError(error);
+      		assert.ok(false);
+    	});
+		
+		sessionConfig.store;
+		
+		return session(sessionConfig);
+};
