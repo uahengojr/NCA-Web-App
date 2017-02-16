7c38f0996d652b572a3ff834f75bf9e317cb0ea1
uahengojr <uahengo@me.com>
2017-02-16 00:06:28 -0800

    Update route handler.

--- WebApp/controllers/profile.js
+++ WebApp/controllers/profile.js
@@ -5,7 +5,6 @@ var ProfileModel = require('../models/profile');
 var User = require('../models/user');
 var easySession = require('easy-session');
 
-
 module.exports = function (router) {
 
     var model = new ProfileModel();
@@ -14,17 +13,16 @@ module.exports = function (router) {
         
         res.render('profile', model);
         
-        
     });
 
-
 	router.get('/users/:id*?', easySession.isLoggedIn(), function(req, res){
+		
 		if(!req.params.id){ 
 			//Use session varibale instead of get variable
 			User.find({_id: req.session.userID}, function(err, owner){
 				
 				if(err){
-					throw err;
+					throw err; //Handle better later...
 				}
 				console.log("Hello One");
 				console.log(req.params.userID);
@@ -33,18 +31,18 @@ module.exports = function (router) {
 				
 				res.render('profile', model);	
 				return;
-			});
+			}).limit(1);
 			
 		}else{
 	
 		var params = {
 			userId: req.session.userID,
-			ownerId: req.params.id
+			ownerId: req.params.id //Owner of the object?
 		};
 		
 		req.session.can('account', params, function(err, has){
 			if(err || !has){
-				res.sendStarus(403);
+				res.sendStatus(403);
 				return;
 			}
 			User.find({_id: params.userId}, function(err, owner){
@@ -53,25 +51,18 @@ module.exports = function (router) {
 				}
 				
 				var model = {ownr: owner};  
-				//console.log(owner);
+				
 				console.log("Hello Two");
+				console.log("\n");
+				console.log(owner);
+				
 				res.render('profile', model);	
 				return;
-			});
+			}).limit(1);
 			
 		});
 	}
-
-/*	
-		var role = req.session.getRole();
-		console.log("The role of the user is: " + role);
-		console.log(req.session)
-		console.log("============");
-		console.log(req.session.isLoggedIn());
-		console.log("============");
 		
-		res.render('profile', model);	
-*/		
 	});
 
 };
