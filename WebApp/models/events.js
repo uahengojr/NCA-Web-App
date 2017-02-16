839e77478de2c64a71a6ad188952f0c5a0fa3397
uahengojr <uahengo@me.com>
2017-02-16 01:42:23 -0800

    Creat events model.

--- WebApp/models/events.js
+++ WebApp/models/events.js
@@ -1,7 +1,47 @@
 'use strict';
 
-module.exports = function EventsModel() {
-    return {
-        name: 'events'
-    };
+var mongoose = require('mongoose');
+
+var event = function(){
+	
+	var eventSchema = mongoose.Schema({
+		
+		eventTitle: {
+			majorTitle: String, 
+			subTitle: String
+		},
+		creator: Object,
+		imgLink: String,
+		eventDescription: Buffer,
+		location:{
+			description: String,
+			address: String, //Maybe make this a geoLocation property????
+			city: String
+		},
+		phoneNumber1: String,
+		eMail1: String,
+		phoneNumber2: String,
+		eMail2: String,
+		eventDate: {
+			type: Date
+		},
+		dateCreated: {
+			type: Date, default: Date.now
+		}, 
+		published: {
+			type: Boolean defaults: true
+		}
+	
+	});
+	
+	/*
+	eventSchema.methods.publish('save', function(next){
+		var creator = this;
+	});
+	*/
+	
+	return mongoose.model('Event', eventSchema);
+	
 };
+
+module.exports = new event();
