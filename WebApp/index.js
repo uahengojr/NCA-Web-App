39a18cabb787cfa3fa7ae616023a687b4f7bd92e
uahengojr <uahengo@me.com>
2017-03-28 20:24:48 -0700

    Update the configuration order.

--- WebApp/index.js
+++ WebApp/index.js
@@ -6,36 +6,13 @@ var kraken = require('kraken-js');
 var db = require('./lib/db');
 var crypto = require('./lib/crypto');
 
+var session = require('express-session'); //Run -> npm install
+var easySession = require('easy-session'); //Run -> npm install
 var passport = require('passport');
-var signUpStrategy= require('./lib/registration');//??
+
+var signUpStrategy= require('./lib/registration');
 var signInStrategy = require('./lib/login');
-
-//
-var session = require('express-session');
-						//--------->     //var session = require('./lib/session');
-var MongoDBStore = require('connect-mongodb-session')(session);
-var easySession = require('easy-session'); // Require the module : line 1
-//
-
-
-var rbac = {
-  user: { // Role name
-    can: ['account', 'post:add', { // list of allowed operations
-      name: 'post:save',
-      when: function (params, callback) {
-        setImmediate(callback, null, params.userId === params.ownerId);
-      }}
-    ]
-  },
-  manager: {
-    can: ['post:save', 'post:delete'],
-    inherits: ['user']
-  },
-  admin: {
-    can: ['rule the server', 'view:dashboard'],
-    inherits: ['manager']
-  }
-}
+var access_rights = require('./lib/ac');
 
 var options, app;
 
@@ -45,67 +22,56 @@ var options, app;
  */
 
 
-options = {
-    onconfig: function (config, next) {
-        /*
-         * Add any additional config setup or overrides here. `config` is an initialized
-         * `confit` (https://github.com/krakenjs/confit/) configuration object.
-         */
+options = function spec(app){
+	
+	app.on('middleware:after:session', function configPassport(eventargs){
 		
-		signUpStrategy(passport); //???
+		app.use(easySession.main(session,{
+			ipCheck: true,
+			uaCheck: true,
+			rbace: access_rights
+		}));
+		
+		//
+		signUpStrategy(passport); 
 		signInStrategy(passport);
 		
-		db.config(config.get('databaseConfig'));
-		var cryptConfig = config.get('bcrypt');
-		crypto.setCryptoLevel(cryptConfig.difficulty);
+		//
+		app.use(passport.initialize());
+		app.use(passport.session());
+		app.use(flash());
 		
-        
-		next(null, config);
-    }
+		//
+		app.use(function(req, res, next) {
+		  res.locals.messages = req.flash();
+		  next();
+		});
+		
+	});
+	
+	return {
+	    onconfig: function (config, next) {
+	        /*
+	         * Add any additional config setup or overrides here. `config` is an initialized
+	         * `confit` (https://github.com/krakenjs/confit/) configuration object.
+	         */
+		
+		
+			db.config(config.get('databaseConfig'));
+			var cryptConfig = config.get('bcrypt');
+			crypto.setCryptoLevel(cryptConfig.difficulty);
+		
+
+			next(null, config);
+	    }
+	};
+    
 };
 
 app = module.exports = express();
-//
-
-var store = new MongoDBStore({
-	uri: 'mongodb://localhost:27017/NCA_Session_Store',
-	collection: 'NCA_Sessions',
-	function(err){
-		console.log(err);
-	}
-});
-/*
-// Catch MONGO CONNECTION errors
-store.on('error', function(error) {
-    assert.ifError(error);
-    assert.ok(false);
-});
-*/
-app.use(session({
-    secret: 'keyboard cat',
-	cookie: {
-		maxAge: 1000*60*60*24*7 //1 week
-	}, 
-	store: store,
-    resave: true,
-    saveUninitialized: false
-}));
-
-//
-app.use(easySession.main(session, {
-	ipCheck: true, 
-	uaCheck: true, 
-	rbac: rbac
-})); // Bind the module : line 2
-//
-
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(flash());
-
 
 app.use(kraken(options));
+
 app.on('start', function () {
     console.log('Application ready to serve requests.');
     console.log('Environment: %s', app.kraken.get('env:env'));
