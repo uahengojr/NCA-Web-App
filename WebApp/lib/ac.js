5c744f5a66bd232c45fe694094dad26b83308b32
uahengojr <uahengo@me.com>
2017-03-28 20:27:29 -0700

    Add access control rights rules.

---
+++ WebApp/lib/ac.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var access_control = function(){
+	
+	return {		
+		  user: { // Role name
+		    can: ['account', 'event:post', 'image:post', { // list of allowed operations
+		      name: 'event:delete', 
+		      when: function (params, callback) {
+		        setImmediate(callback, null, params.userId === params.ownerId);
+		      }}, {
+				  name: 'image:delete',
+				  when: function(params, callback){
+					setImmediate(callback, null, params.userId === params.ownerId);
+			   }}
+		    ]
+		  },
+		  board: {
+		    can: ['event:save', 'event:delete', 'donation:post', 'donation:delete', 'donation:save', 'news:post', 'news:delete', 'news:save', 'view:dashboard'],
+		    inherits: ['user']
+		  },
+		  admin: {
+		    can: ['rule the server'],
+		    inherits: ['manager']
+		  }
+	}
+	
+};
+
+module.exports = access_control();
