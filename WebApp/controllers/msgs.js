8f298b898d882112b528ae87bf31d14bda19a5cf
uahengojr <uahengo@me.com>
2017-03-30 13:04:24 -0700

    Update the messages route handlers.

--- WebApp/controllers/msgs.js
+++ WebApp/controllers/msgs.js
@@ -6,12 +6,36 @@ var easySession = require('easy-session');
 module.exports = function (router) {
 
     var model = new MsgsModel();
-
-    router.get('/', easySession.isLoggedIn(), function (req, res) {
+	
+	///Figure out how to best handle message!!!
+	
+    router.get('/', easySession.isLoggedIn(), function(req, res) {
         
+		if(req.session.hasRole(['board', 'admin', 'user'])) {
+			
+			console.log(req.session.userID);
+			
+			req.session.can('account', function(err, has){
+				if(err || !has){
+					res.sendStatus(403);
+				}
+				
+				Msgs.find({_id: req.session.userID}, function(err, msgs){
+					if(err){
+						console.error(err);
+					}
+					
+					//Check if ther ar ethe owner of the messages again here???
+					var model = {msgs: msgs};
+					
+					res.render('msgs', model);
+					
+				});
+			});
+			
+		}
 		
-        res.render('msgs', model);
-        
+		//else redirect them to login??
         
     });
 
