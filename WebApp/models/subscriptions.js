27e051f619e3993b3b87e2dc2a2a95e9522f5fc5
uahengojr <uahengo@me.com>
2017-03-30 15:52:00 -0700

    Update the subscription model.

--- WebApp/models/subscriptions.js
+++ WebApp/models/subscriptions.js
@@ -1,7 +1,78 @@
 'use strict';
 
-module.exports = function SubscriptionsModel() {
-    return {
-        name: 'subscriptions'
-    };
+var mongoose = require('mongoose');
+
+var Subscriptions = function() {
+    
+	/* - Defines Subscription Schema - */
+	var Subscription = mongoose.Schema({
+		
+		name: {type: String, required: true},
+		amount: {type: String, required: true},
+		description: {type: Buffer, required: true},
+		num_of_subscribers: [{userID: ObjectId, subscription_type: String, amount: Number}],
+		total_subscription_revenue: {type: Number, min: 0, default: 0, required: true},
+		expiry_date: {type: Date, required: true},
+		date_created: {type: Date, default: Date.now}
+		
+	});
+	
+	// - Helper functions - //
+
+	Subscription.methods.canceled_subscription = function(subscriber){
+		if(this.num_of_subscribers.length === 0){
+			
+			return;
+			
+		}else{
+			
+			var removeSubscriber = {
+				userID: subscriber[ID],
+				subscription_type: subscriber[type],
+				amount: subscriber[amount]
+			};
+			
+			this.num_of_subscribers.pop(removeSubscriber[userID]); 
+			return;
+			
+		}
+		
+	};
+	
+	//Counters for total number of subscribers
+	Subscription.methods.new_subscription = function(subscriber){
+		if(this.num_of_subscribers.length === 0){
+			
+			return;
+			
+		}else{
+			
+			var newSubscriber = {
+				userID: subscriber[ID],
+				subscription_type: subscriber[type],
+				amount: subscriber[amount]
+			};
+			
+			this.num_of_subscribers.push(newSubscriber);
+			return;
+		}
+	};
+	
+	Subscription.methods.addAmount = function(amount){
+		if(this.total_subscription_revenue === 0 || this.total_subscription_revenue < 0){
+			
+			this.total_subscription_revenue = 0; //A lock must be attained to avoid race conditions.
+			return;
+			
+		}else{
+		
+			this.total_subscription_revenue += amount;
+			return;
+		}
+	};
+	
+	return mongoose.model('Subscription', Subscription);
 };
+
+
+module.exports = new Subscriptions();
