c46eca7cd9d31178d3beb186d365824934724316
uahengojr <uahengo@me.com>
2017-03-31 01:28:40 -0700

    I don't recall what I schanged.

--- WebApp/controllers/donations.js
+++ WebApp/controllers/donations.js
@@ -1,18 +1,68 @@
 'use strict';
 
-var DonationsModel = require('../models/donations');
+var Donation = require('../models/donations');
 var easySession = require('easy-session');
 
 module.exports = function (router) {
 
-    var model = new DonationsModel();
-
     router.get('/', easySession.isLoggedIn(), function (req, res) {
         
+        if(req.session.hasRole(['admin','board', 'user'])){
+			
+			Donation.find({}, function(err, donations){
+				
+				if(err){
+					return console.error(err);
+				}
+				
+				var model = {donations: donations};
+			
+				res.render('donations', model);
+				
+			});
+        	
+        }else{
+			
+        	return res.render('errors/404', {url:req.url});
+			
+        }
         
-        res.render('donations', model);
-        
-        
+    }).post('/:donationID', easySession.isLoggedIn(), function(req, res){
+		
+		if(req.session.hasRole(['admin','board', 'user'])){
+    			
+			Donation.updateOne({_id: req.params.donationID}, function(err, donation){
+				
+				if(err){
+					return console.error(err);
+				}
+				req.session.can('account', function(err, has){
+					
+					if(err || !has){
+						return res.sendStatus(403);
+					}
+					
+					
+					var donor = {
+						ID: req.session.userID,
+						amount: req.body.amount
+					};
+					
+					donation.addDonor(donor); 
+					donation.addRevenue(donor[amount]);
+					
+					return res.send('Success' /*Some flash message here...*/);
+					
+				});
+				
+			});
+			
+    	}else{
+			
+        	return res.render('errors/404', {url:req.url});
+			
+        }
+		
     });
 
 };
