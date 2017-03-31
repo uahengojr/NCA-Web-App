45055f1d4c843f801cf799c2986c4aa3f810d3a0
uahengojr <uahengo@me.com>
2017-03-30 19:07:06 -0700

    Update route handler logic.

--- WebApp/controllers/subscriptions.js
+++ WebApp/controllers/subscriptions.js
@@ -1,6 +1,8 @@
 'use strict';
 
-var User = require('../model/user');
+var User = require('../model/user'),
+	Subscription = require('../model/subscription');
+
 var easySession = require('easy-session');
 
 module.exports = function (router) {
@@ -12,9 +14,9 @@ module.exports = function (router) {
 			User.find({_id:req.session.userID}, function(err, owner){
 			
 				if(err){
-					return console.error(err); //Send your self some for info about this error?
+					return console.error(err); //Send yourself some for info about this error?
 				}
-			
+			/*
 				var model = {
 					subscriptions: subs
 				};
@@ -23,14 +25,14 @@ module.exports = function (router) {
 					visitorID: req.session.userID,
 					ownerID: owner[0]._id
 				};
-			
-				req.session.can('account', params, function(err, has){
+			*/
+				req.session.can('account',/* params,*/ function(err, has){
 				
 					if(err || !has){
 						return res.sendStatus(403);
 					}
 				
-					res.render('subscriptions', model);
+					return res.render('subscriptions');
 				
 				});
 			
@@ -42,6 +44,43 @@ module.exports = function (router) {
 			
 		}
         
+    }).post('/:id', easySession.isLoggedIn(), function(req, res){
+    	
+		if(req.session.hasRole(['admin', 'board', 'user'])){
+						
+			Subscription.update({_id:req.params.subscripitonID}, function(err, subscription){
+				
+				if(err){
+					return console.error(err);
+				}
+				
+				req.session.can('account', function(err, has){
+					
+					if(err || !has){
+						return res.sendStatus(403);
+					}
+					
+					var newSubscriber = {
+						ID: req.session.userID,
+						type: req.body.type,
+						amount: req.body.amount
+					};
+					
+					subscription.new_subscription(newSubscriber); //Tally subscriber number.
+					subscription.addAmount(newSubscriber[amount]); //Tally total revenue received to date.
+					
+					return res.send('success'/* - Some req.flash message here - */);
+					
+				});
+				
+			});
+			
+		}else{
+			
+			return res.render('errors/404', {url:req.url});
+			
+		}
+		
     });
 
 };
