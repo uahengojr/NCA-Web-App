31edfd4c2682c3b7718d520b8e6c3ada52210167
uahengojr <uahengo@me.com>
2017-07-19 03:04:34 -0700

    Add route authorization logic.

---
+++ WebApp/lib/auth.js
@@ -0,0 +1,28 @@
+'use strict';
+var easySession = require('easy-session');
+
+var reqRoute = function() {
+	return function(req, res, next){
+				
+		if( req.session.isLoggedIn() ) {
+			
+			if(req.session.hasRole(['user', 'board','admin'])){
+				console.log('User is logged in & has a defined role.');
+				next();
+			}else{
+				//You have a different role, if you reach this...
+			}
+			
+		}else{
+			req.session.goingTo = req.url;
+			req.flash('error', 'Please sign-in to view the requested page.');
+			res.redirect('/signIn');
+		}
+	};
+};
+
+module.exports = reqRoute();
+
+
+
+
