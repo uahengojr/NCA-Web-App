c75f98f077951298f1780261178a433353f33578
uahengojr <uahengo@me.com>
2017-07-22 11:55:21 -0700

    Update the profile route handler with resoruce check function.

--- WebApp/controllers/profile.js
+++ WebApp/controllers/profile.js
@@ -1,64 +1,108 @@
 'use strict';
 
-//var ProfileModel = require('../models/profile');
-
 var User = require('../models/user');
 var easySession = require('easy-session');
 
 //Middleware
 var auth = require('../lib/auth');
+var resourceCheck = require('../lib/resourceCheck');
 
 module.exports = function (router) {
-
-    //var model = new ProfileModel();
 
 	router.get('/:id*?', auth(), easySession.isLoggedIn(), function(req, res){
 		
 		if(!req.params.id){ 
 			//Use session varibale instead of get variable
-			User.find({_id: req.session.userID}, {role: 1, name: 1, _id: 0}, function(err, owner){
+			User.findOne({_id: req.session.userID}, {role: 1, name: 1}, function(err, owner){
 				
 				if(err){
+					console.error(err);
 					throw err; //Handle better later...
 				}
+				var params = {
+					userID: req.session.userID,
+					ownerID: owner.id
+				};				
 				
-				console.log("Hello One");
-				var model = {ownr: owner}; 
-								
-				res.render('profile', model);	
+				if(new resourceCheck(req, params)){
+					var model = {user: owner};  
+					return res.render('profile', model);
+					
+				}
+				
+				if(!(new resourceCheck(req, params))){
+					return res.sendStatus(403);
+				}
+				
+				/*
+				req.session.can('account', params, function(err, has){
+					if(err){ 
+						//something went wrong with the check. Log error
+						res.sendStatus(403);
+						return;
+					}
+					
+					if(!has){
+						//User not allowed. 
+						return res.sendStatus(403);
+					}
+					
+					var model = {user: owner};
+					
+					return res.render('profile', model);
+					
+				});
+				*/
 
-			}).limit(1);
+			});
 			
 		}
 		
-		if(req.session.userID === req.params.id || req.session.role === 'admin' ){
-	
-			var params = {
-				userID: req.session.userID,
-				ownerID: req.params.id //Owner of the object?
-			};
-						
-			req.session.can('account', params, function(err, has){
-				if(err || !has){
-					return res.sendStatus(403);
-				}
-			
-		        User.find({_id: params.ownerID}, {role: 1, name: 1, _id: 0}, function(err, owner){
+		if(req.session.userID === req.params.id){
+				
+		        User.findOne({_id: req.params.id}, {role: 1, name: 1}, function(err, owner){
 					if(err){
 						console.error(err);
 						throw err;
 					}
+					
+					var params = {
+						userID: req.session.userID,
+						ownerID: owner.id
+					};
+									
+					if(new resourceCheck(req, params)){
+						var model = {user: owner};  
+						return res.render('profile', model);
+						
+					}
+					
+					if(!(new resourceCheck(req, params))){
+						return res.sendStatus(403);
+					}
+					
+					/*	
+					req.session.can('account', params, function(err, has){
+						if(err){ 
+						
+							//something went wrong with teh check
+							res.sendStatus(403);
+							return;
+						}
+					
+						if(!has){
+							//Not allowed
+							return res.sendStatus(403);
+						}
+						
+						var model = {user: owner};  
 		
-					var model = {ownr: owner};  
-				
-					console.log("Hello Two");
-					console.log(owner);
-		
-					res.render('profile', model);
+						return res.render('profile', model);
+						
+					});
+					*/
 		
 				});	
-				
-			});
 			
 		}
 		
