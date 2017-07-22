ca9b558aaa5a7f578b5a8df00c1eaaf64c137273
uahengojr <uahengo@me.com>
2017-07-22 13:10:46 -0700

    Remove admin model.

--- WebApp/controllers/events.js
+++ WebApp/controllers/events.js
@@ -1,7 +1,7 @@
 'use strict';
 
+var Event = require('../models/events');
 var easySession = require('easy-session');
-var events = require('../models/events');
 
 //Middleware
 var auth = require('../lib/auth');
@@ -10,10 +10,86 @@ module.exports = function (router) {
 
     router.get('/', auth(), easySession.isLoggedIn(), function (req, res) {
         
+		if(req.session.hasRole(['board', 'user'])){
+			
+			Event.find({}, {sort: {eventDate: -1} }, function(err, events){
+				
+				if(err){
+					return console.error(err);
+				}
+				
+				var model = {events: events};
+				
+				res.render('events', model);
+				
+			});
+			
+		}else{
+			
+			
+			return res.render('errors/404', {url:req.url});
+			
+		}
         
-        res.render('events', model);
-        
-        
-    });
+    }).get('/:id', auth(), easySession.isLoggedIn(), function(req, res){
+		
+		if(req.session.hasRole(['board', 'user'])){
+			
+			Event.findOne(function(err, event){
+				
+				if(err){
+					return console.error(err);
+				}
+				
+				var model = {event: event};
+				
+				res.render('event', model);
+				
+			});
+			
+		}else{
+			
+			
+			return res.render('errors/404', {url:req.url});
+			
+		}
+		
+    }).post('/', auth(), easySession.isLoggedIn(), function(req, res){
+		
+    	if(req.session.hasRole(['board', 'user'])){
+			
+    		Event.insertOne(function(err, result){
+    			if(err){ 
+					return console.error(err);
+				}
+				var newEvent = new Event();
+				
+				//Blah Blah Blah...
+				/**	newEvent.
+					newEvent.
+					newEvent.
+					newEvent.
+					newEvent.
+					newEvent.
+					newEvent.
+					newEvent.
+				**/
+				newEvent.save(function(err, event){
+					if(err){
+						return console.error(err);
+					}
+					
+					return res.redirect('events/:' + event.id, req.flash('success','Welcome to NCA the community!'));
+					
+				});
+    		});
+			
+    	}else{
+			
+			return res.render('errors/404', {url:req.url});
+			
+    	}
+		
+    }).put().delete();
 
 };
