7df8aa0d4b684632e6bceeb17b17f1ed1c7dfd7d
uahengojr <uahengo@me.com>
2017-07-22 12:01:07 -0700

    Delete profile and edit profiel models.

--- WebApp/controllers/events.js
+++ WebApp/controllers/events.js
@@ -1,14 +1,12 @@
 'use strict';
 
-var EventsModel = require('../models/events');
 var easySession = require('easy-session');
+var events = require('../models/events');
 
 //Middleware
 var auth = require('../lib/auth');
 
 module.exports = function (router) {
-
-    var model = new EventsModel();
 
     router.get('/', auth(), easySession.isLoggedIn(), function (req, res) {
         
--- WebApp/models/editprofile.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function EditprofileModel() {
-    return {
-        name: 'editprofile'
-    };
-};
