cb7469319ab51f3a84a8d8eb86f4449a437563b5
uahengojr <uahengo@me.com>
2017-07-22 11:54:57 -0700

    Update the billing route handler with resoruce check function.

--- WebApp/controllers/billing.js
+++ WebApp/controllers/billing.js
@@ -2,80 +2,101 @@
 
 var BillingModel = require('../models/billing');
 var easySession = require('easy-session');
+var User = require('../models/user');
 
 //Middleware
 var auth = require('../lib/auth');
 
 module.exports = function (router) {
 
-    router.get('/', auth(), easySession.isLoggedIn(), function (req, res) {
+    router.get('/:id*?', auth(), easySession.isLoggedIn(), function (req, res) {
         
-		if(req.session.hasRole(['admin', 'board', 'user'])) {
-			
-			User.find({_id: req.session.userID}, function(err, billing){
+		if(!req.params.id){
+			User.findOne({_id: req.session.userID}, {role: 1, name: 1}, function(err, billing){
 				
 				if(err){
 					return console.error(err);
 				}
-				/*
-				var model = {
-					billing: billing
+				var params = {
+					userID: req.session.userID,
+					ownderID: billing._id
 				};
 				
-				var params = {
-					visitorID: req.session.userID,
-					ownerID: billing[0]._id
-				};
-				*/
-				req.session.can('account', function(err, has){
+				if(new resourceCheck(req, params)){
+					var model = {user: billing};  
+					return res.render('billing', model);
 					
-					if(err || !has){
-						return res.sendStatus(403);
-					}
-					
-					return res.render('billing');
-					
-				});
+				}
+				
+				if(!(new resourceCheck(req, params))){
+					return res.sendStatus(403);
+				}
 				
 			});
+        }
+	/*	
+		if(req.session.userID === req.params.id){
 			
-		}else{
-			
-			return res.render('errors/404', {url:req.url});
-			
+			User.find({_id: req.params.id}, function(err, billing){
+				if(err){
+					return console.error(err);
+				}
+				
+				var params = {
+					userID: req.session.userID,
+					ownerID: billing._id
+				};
+		
+				if(new resourceCheck(req, params)){
+					var model = {user: owner};  
+					return res.render('profile', model);
+					
+				}
+				
+				if(!(new resourceCheck(req, params))){
+					return res.sendStatus(403);
+				}
+		
+				req.session.can('account', params, function(err, has){
+					if(err || !has){
+						return sendStatus(403);
+					}
+					var billing = {
+						billing: billing
+					};
+					
+					return res.render('billing', model);
+				});
+		
+			});
+		
 		}
-        
+    */
     });
 	
     router.get('/payment', auth(), easySession.isLoggedIn(), function (req, res) {
         
-        if(req.session.hasRole(['admin', 'board', 'user'])){
+        if(req.session.hasRole(['board', 'user'])){
         	
-			User.find({_id: req.session.userID}, function(err, payment){
+			User.findOne({_id: req.session.userID}, function(err, payment){
 				
 				if(err){
 					return console.error(err);
 				}
-				/*
-				var model = {
-					payment: payment
+				var params = {
+					userID: req.session.userID,
+					ownderID: payment._id
 				};
 				
-				var params = {
-					visitorID: req.session.userID,
-					ownerID: payment[0]._id
-				};
-				*/
-				req.session.can('account', function(err, has){
+				if(new resourceCheck(req, params)){
+					var model = {user: payment};  
+					return res.render('payment', model);
 					
-					if(err || !has){
-						return res.sendStatus(403);
-					}
-					
-					return res.render('payment');
-					
-				});
-			});
+				}
+				
+				if(!(new resourceCheck(req, params))){
+					return res.sendStatus(403);
+				}
 			
         }else{
         	
@@ -87,31 +108,27 @@ module.exports = function (router) {
 	
     router.get('/editcard', auth(), easySession.isLoggedIn(), function (req, res) {
         
-        if(req.session.hasRole(['admin', 'board', 'user'])){
+        if(req.session.hasRole(['board', 'user'])){
         	
-			User.find({_id: req.session.userID}, function(err, cardEdit){
+			User.findOne({_id: req.session.userID}, function(err, cardEdit){
 				if(err){
 					return console.error(err);
 				}
-				/*
-				var model = {
-					cardEdit: cardEdit
-				};
 				
 				var params = {
-					visitorID: req.session.userID,
-					ownerID: cardEdit[0]._id
+					userID: req.session.userID,
+					ownderID: cardEdit._id
 				};
-				*/
-				req.session.can('account', function(err, has){
+				
+				if(new resourceCheck(req, params)){
+					var model = {user: cardEdit};  
+					return res.render('cardEdit', model);
 					
-					if(err || !has){
-						return res.sendStatus(403);
-					}
-					
-					return res.render('editcard');
-					
-				});
+				}
+				
+				if(!(new resourceCheck(req, params))){
+					return res.sendStatus(403);
+				}
 			
 			});
 			
