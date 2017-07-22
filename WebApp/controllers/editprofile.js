ecbabdaf04308d90964254116f834ce4610b6b88
uahengojr <uahengo@me.com>
2017-07-22 11:55:39 -0700

    Update the profile edit route handler with resoruce check function.

--- WebApp/controllers/editprofile.js
+++ WebApp/controllers/editprofile.js
@@ -1,6 +1,5 @@
 'use strict';
 
-var EditprofileModel = require('../models/editprofile');
 var easySession = require('easy-session');
 
 //Middleware
@@ -8,14 +7,42 @@ var auth = require('../lib/auth');
 
 module.exports = function (router) {
 
-    var model = new EditprofileModel();
-
     router.get('/', auth(), easySession.isLoggedIn(), function (req, res) {
-        
-        
-        res.render('editprofile', model);
-        
-        
+		
+		if(req.session.hasRole(['board', 'user']) && req.session.userID) {
+			//Use session varibale instead of get variable
+			User.findOne({_id: req.session.userID}, {role: 1, name: 1}, function(err, profile){
+				
+				if(err){
+					console.error(err);
+					throw err; //Handle better later...
+				}
+				var params = {
+					userID: req.session.userID,
+					ownerID: profile.id
+				};				
+				
+				if(new resourceCheck(req, params)){
+					var model = {user: profile};  
+					return res.render('editprofile', model);
+					
+				}
+				
+				if(!(new resourceCheck(req, params))){
+					return res.sendStatus(403);
+				}
+
+			});
+			
+		}else{
+			
+			return res.render('errors/404', {url:req.url});
+			
+		}
+		
+           
     });
+	
+	
 
 };
