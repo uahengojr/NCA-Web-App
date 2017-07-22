2db677ccc9748152f2469da5e202f65b50b5087e
uahengojr <uahengo@me.com>
2017-07-22 01:31:23 -0700

    Add resource check function

---
+++ WebApp/lib/resourceCheck.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var easySession = require('easy-session');
+
+module.exports = function(req, params){
+		
+	req.session.can('account', params, function(err, has){
+		if(err){
+			console.error(err);	//handle & log the error better, for latter analysis...
+			return false;
+		}
+				
+		if(!has){
+			return false;
+		}else{
+			return true;
+		}
+				
+	});
+	 
+	
+};
+
